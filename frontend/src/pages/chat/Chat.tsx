9e03b78143955e69c18f1057582f80446c8a7a74
Sean Dentener <SDENTENER@capetown.gov.za>
2024-08-27 10:27:33 +0200

    updated peza logo

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -12,7 +12,7 @@ import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
 import { nord } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import styles from './Chat.module.css'
-import Contoso from '../../assets/CCT/PEXA.png'
+import PexaLogo from '../../assets/CCT/PEXA.png'
 import { XSSAllowTags } from '../../constants/sanatizeAllowables'
 
 import {
@@ -107,7 +107,7 @@ const Chat = () => {
 
   useEffect(() => {
     if (!appStateContext?.state.isLoading) {
-      setLogo(ui?.chat_logo || ui?.logo || Contoso)
+      setLogo(PexaLogo)
     }
   }, [appStateContext?.state.isLoading])
 
